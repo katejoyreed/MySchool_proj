96b57991043db99e3ce6f87dd97cd4b631e2a50e
katejoyreed <katejoyreed@gmail.com>
2021-04-24 12:52:48 -0500

    Saffolded ParentsController w/ views

---
+++ MySchool/MySchool/Controllers/ParentsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MySchool.Data;
+using MySchool.Models;
+
+namespace MySchool.Controllers
+{
+    public class ParentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ParentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Parents
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Parents.Include(p => p.IdentityUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Parents/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parent = await _context.Parents
+                .Include(p => p.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (parent == null)
+            {
+                return NotFound();
+            }
+
+            return View(parent);
+        }
+
+        // GET: Parents/Create
+        public IActionResult Create()
+        {
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Parents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,PreferredTitle,PhoneNumber,Email,Address,IdentityUserId")] Parent parent)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(parent);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", parent.IdentityUserId);
+            return View(parent);
+        }
+
+        // GET: Parents/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parent = await _context.Parents.FindAsync(id);
+            if (parent == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", parent.IdentityUserId);
+            return View(parent);
+        }
+
+        // POST: Parents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,PreferredTitle,PhoneNumber,Email,Address,IdentityUserId")] Parent parent)
+        {
+            if (id != parent.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(parent);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ParentExists(parent.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", parent.IdentityUserId);
+            return View(parent);
+        }
+
+        // GET: Parents/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var parent = await _context.Parents
+                .Include(p => p.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (parent == null)
+            {
+                return NotFound();
+            }
+
+            return View(parent);
+        }
+
+        // POST: Parents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var parent = await _context.Parents.FindAsync(id);
+            _context.Parents.Remove(parent);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ParentExists(int id)
+        {
+            return _context.Parents.Any(e => e.Id == id);
+        }
+    }
+}
